f2ad0aa34d59f430fa23d2db93e9c6ed274c3e05
Amr Elsaaadny <topia.amr@icloud.com>
2025-03-01 19:43:15 +0200

    shimmering

--- lib/features/user_profiles/presentation/widgets/user_profile_widget.dart
+++ lib/features/user_profiles/presentation/widgets/user_profile_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:strichpunkt/features/user_profiles/domain/entities/user_profile.dart';
 import 'package:strichpunkt/features/user_profiles/presentation/cubits/get_user_profiles_cubit.dart';
 import 'package:strichpunkt/features/user_profiles/presentation/cubits/get_user_profiles_state.dart';
 import '../../../../core/theme/app_theme.dart';
@@ -11,6 +13,23 @@ class UserProfileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<UserProfile> mockProfiles = [
+      ...List.generate(
+        5,
+        (index) => UserProfile(
+          id: '1',
+          name: 'Matthew Brian',
+          svnNumber: '123 456 789',
+          avatarUrl: 'assets/images/avatar1.png',
+          relation: 'Me',
+          status: ProfileStatus.pending,
+          connectCount: 2,
+          medicineCount: 11,
+          consultCount: 2,
+          isCurrentlyInUse: true,
+        ),
+      ),
+    ];
     return BlocBuilder<GetUserProfilesCubit, GetUserProfilesState>(
       builder: (context, state) {
         return Scaffold(
@@ -33,21 +52,21 @@ class UserProfileWidget extends StatelessWidget {
           ),
           body: Builder(
             builder: (context) {
-              if (state.isLoading) {
-                return const Center(child: CircularProgressIndicator());
-              }
               if (state.isError) return Center(child: Text(state.message));
-              return ProfilesList(
-                profiles: state.profiles,
-                onEdit: (profile) {
-                  // Handle edit profile
-                },
-                onSwitchProfile: (profile) {
-                  // Handle switch profile
-                },
-                onConnection: (profile) {
-                  // Handle connection
-                },
+              return Skeletonizer(
+                enabled: state.isLoading,
+                child: ProfilesList(
+                  profiles: state.isLoading ? mockProfiles : state.profiles,
+                  onEdit: (profile) {
+                    // Handle edit profile
+                  },
+                  onSwitchProfile: (profile) {
+                    // Handle switch profile
+                  },
+                  onConnection: (profile) {
+                    // Handle connection
+                  },
+                ),
               );
             },
           ),
